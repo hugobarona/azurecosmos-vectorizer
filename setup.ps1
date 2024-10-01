8f94213df21e00bf9f74ac94008c5ebc22202c14
Hugo Barona <hugofct20257@gmail.com>
2024-10-01 22:16:03 +0100

    Initial commit

---
+++ setup.ps1
@@ -0,0 +1,5 @@
+RES_GROUP=<resource-group-name>
+ACCT_NAME=<cosmos-db-account-name>
+
+export ACCOUNT_URI=$(az cosmosdb show --resource-group $RES_GROUP --name $ACCT_NAME --query documentEndpoint --output tsv)
+export ACCOUNT_KEY=$(az cosmosdb list-keys --resource-group $RES_GROUP --name $ACCT_NAME --query primaryMasterKey --output tsv)
